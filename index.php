29aa1e65a6715b8e1739a4064ae09ed01100292e
Katie Volz <iggyvolz@gmail.com>
2020-06-25 18:20:45 -0400

    update because i'm bad at things

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
 <?php
-require_once "config.php";
-require_once __DIR__.'/vendor/autoload.php';
-use RestCord\DiscordClient;
-$items=array_merge(...array_map(function(string $playlist):array{
-	$litems=[];
-	$pagetoken="";
-	do
-	{
-		$conts=json_decode(file_get_contents("https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=$playlist&key=".YOUTUBE_TOKEN.$pagetoken));
-		$litems[]=$conts->items;
-		if($conts->nextPageToken) {
-			$pagetoken="&pageToken=".$conts->nextPageToken;
-		} else {
-			$pagetoken=null;
-		}
-	}
-	while($pagetoken);
-	return array_merge(...$litems);
-},YOUTUBE_PLAYLISTS));
-$count=count($items);
-$index=random_int(0,$count);
-$videoid=$items[$index]->contentDetails->videoId;
-if(NO_DISCORD_PING)
-{
-	echo "Log in to discord with token ".DISCORD_TOKEN.", message ".DISCORD_CHANNEL." with 'VGM of the Day - ".date("m/d/y")." - https://www.youtube.com/watch?v=$videoid'".PHP_EOL;
-}
-else
-{
-	$discord = new DiscordClient(['token' => DISCORD_TOKEN]); // Token is required
-	$discord->channel->createMessage(['channel.id' => DISCORD_CHANNEL, 'content' => 'VGM of the Day - '.date("m/d/y").' https://www.youtube.com/watch?v='.$videoid]);
-}
+$config = json_decode(file_get_contents(__DIR__ . "/config.json"), true);
+$messages = file(__DIR__ . "/messages.txt");
+// Trim each line
+$messages = array_map("trim", $messages);
+// Remove empty lines and lines starting with a comment
+$messages = array_values(array_filter($messages, fn(string $s):bool => strlen($s) !== 0 && $s[0] !== ";"));
+$index=random_int(0,count($messages)-1);
+$message=$messages[$index];
+$curl = curl_init($config["webhook"]);
+curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-Type:application/json"]);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true );
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode([
+	"text" => $message
+]));
+curl_exec($curl);
